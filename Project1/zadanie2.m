cfe8cfe86386b61a397b0237fc228465ea1f50ba
Batek Boczek <goterix1@gmail.com>
2017-03-07 16:21:29 +0200

    Progress

--- Project1/zadanie2.m
+++ Project1/zadanie2.m
@@ -1,26 +1,32 @@
 clear all;
-jumps = [1.3,1.2,1.0,0.9];
+jumps = [1.2,1.0,0.9,1.3];
  
 Upp=1.1;
-%Ypp=2;
- 
-for k=jumps
+Ypp=2;
+% U=zeros(150);
+% Y=zeros(150);
+for j=jumps
  
     for i=1:150
-        U(i)=k;
-        Y(i)=0;
+        U(i)=j;
+        Y(i)=Ypp;
     end
- 
-    U(1)=Upp;
+     U(1)=Upp;
+%     U(2:end)=k;
+%     Y(1:end)=Ypp;
+    
  
     for k=12:1:150
         Y(k)=symulacja_obiektu3Y(U(k-10),U(k-11),Y(k-1),Y(k-2));
     end
  
-    %fclose(dane)
+    
     plot(Y)
-    hold on;
+    nazwa = strcat('zadanie2_jump=', num2str(j),'.txt');
+    savePlot(U,Y,nazwa);
+    hold on;    
 end
+
 title('odpowiedzi skokowe')
 legend('skok U do 1.3','skok U do 1,2','skok U do 1.0','skok U do 0.9');
 % dane = fopen('dane2.txt','wt');
@@ -29,18 +35,19 @@ legend('skok U do 1.3','skok U do 1,2','skok U do 1.0','skok U do 0.9');
 %     fprintf(dane,'%f %f\n',U(i),Y(i));
 % end
 % fclose(dane);
-savePlot(U,Y,'dane.txt'); 
+
 n=200;
 for i = 1:101
-    Us(i) = 0.9 + 0.4*(i-1)*(1/100);
+    Us(i) = 1.1 + 0.2*(i-1)*(1/100);
     U(1:20) = 1.1;
     U(20:n) = Us(i);
     for k = 12:n
         Y(k) = symulacja_obiektu3Y(U(k-10), U(k-11), Y(k-1), Y(k-2));
     end
     Ys(i) = Y(n);
+   
 end
-Kstat=(Ys(100)-Ys(1))/(Us(100)-Us(1));
+Kstat=(Ys(101)-Ys(1))/(Us(101)-Us(1));
  
 hold off;
 figure
---
+++ Project1/zadanie3.m
@@ -0,0 +1,23 @@
+clear all
+params
+
+%inicjalizacja
+U=zeros(n,1);
+Y=zeros(n-12,1);
+Y(1:12)=Ypp;
+%skok jednostkowy z punktu pracy do wartosci maksymalnej w chwili 20
+U(1:20)=Upp; 
+U(21:end)=Umax;
+ for k = 12:n
+    Y(k) = symulacja_obiektu3Y(U(k-10), U(k-11), Y(k-1), Y(k-2));
+ end
+ 
+ figure
+ plot(Y)
+ 
+ s=zeros(n-21,1); %odpowiedz skokowa
+ for k = 21:n
+    s(k-20) = (Y(k)-Ypp)/(Umax-Upp);
+ end
+ figure
+ stairs(s)
