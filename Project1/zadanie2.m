7ecad1219e454ca5410ede9101ff21d7ffc97db4
Batek Boczek <goterix1@gmail.com>
2017-03-07 12:35:11 +0200

    Task 2,4,5 PID

---
+++ Project1/zadanie2.m
@@ -0,0 +1,49 @@
+clear all;
+jumps = [1.3,1.2,1.0,0.9];
+ 
+Upp=1.1;
+%Ypp=2;
+ 
+for k=jumps
+ 
+    for i=1:150
+        U(i)=k;
+        Y(i)=0;
+    end
+ 
+    U(1)=Upp;
+ 
+    for k=12:1:150
+        Y(k)=symulacja_obiektu3Y(U(k-10),U(k-11),Y(k-1),Y(k-2));
+    end
+ 
+    %fclose(dane)
+    plot(Y)
+    hold on;
+end
+title('odpowiedzi skokowe')
+legend('skok U do 1.3','skok U do 1,2','skok U do 1.0','skok U do 0.9');
+dane = fopen('dane2.txt','wt');
+ 
+for i =1:150
+    fprintf(dane,'%f %f\n',U(i),Y(i));
+end
+fclose(dane);
+ 
+n=200;
+for i = 1:101
+    Us(i) = 0.9 + 0.4*(i-1)*(1/100);
+    U(1:20) = 1.1;
+    U(20:n) = Us(i);
+    for k = 12:n
+        Y(k) = symulacja_obiektu3Y(U(k-10), U(k-11), Y(k-1), Y(k-2));
+    end
+    Ys(i) = Y(n);
+end
+Kstat=(Ys(100)-Ys(1))/(Us(100)-Us(1));
+ 
+hold off;
+figure
+plot(Us,Ys);
+title(Kstat);
+%statyczne s¹, dynamiczne nie, bo jest inercja z opóŸnieniem chyba xD
---
+++ Project1/zadanie45PID.m
@@ -0,0 +1,70 @@
+clear all;
+
+ 
+
+%__init__():
+n = 2500;
+Yzad(1:n) = 1.8;
+Yzad(21:n) = 1.6;
+Yzad(1001:n)=1.4;
+Yzad(1501:n)=1.5;
+Yzad(2201:n)=1.7;
+U(1:n) = 1.0;
+
+%self.punkt_pracy
+Ypp = 2; 
+Upp = 1.1;
+
+Y(1:n) = Ypp;
+u = U - Upp;
+err = 0;
+%end __init__
+
+
+K = 0.6; Ti = 11; Td = 3; Ts = 0.5;
+
+
+r2 = K*Td/Ts;
+r1 = K*(Ts/(2*Ti)-2*Td/Ts - 1);
+r0 = K*(1+Ts/(2*Ti) + Td/Ts);
+%co to wogule jest jakies ruwnania na kiju :---DDD
+
+%'__name__'==='__main__'
+for k=21:n 
+     Y(k)=symulacja_obiektu3Y(U(k-10), U(k-11), Y(k-1), Y(k-2)); 
+     
+     e(k)=Yzad(k)-Y(k);
+     
+     du = r2*e(k-2)+r1*e(k-1)+r0*e(k);
+     
+     if du>0.05 %ograniczenia na szybkosc zmiany sygnalu sterowania
+         du = 0.05;
+     end
+     if du<-0.05
+         du = -0.05;
+     end
+     
+     u(k)=du+u(k-1); 
+     
+     if u(k)>0.5 %ograniczenia na min/max sygnalu sterowania
+         u(k) = 0.5;
+     end
+     if u(k)<-0.5
+         u(k) = -0.5;
+     end
+     U(k)= u(k) + Upp; %przesuniecie sterowania do punktu pracy
+end; 
+
+err = sum(e.^2)
+
+figure('Position',  [403 246 820 420]);
+%title('PID z parametrami eksperymentalnymi, err=19.68');
+subplot('Position', [0.1 0.12 0.8 0.15]);
+stairs(U);
+ylabel('u'); 
+xlabel('k');
+subplot('Position', [0.1 0.37 0.8 0.6]);
+stairs(Y);
+ylabel('y'); 
+hold on; 
+stairs(Yzad,':');
