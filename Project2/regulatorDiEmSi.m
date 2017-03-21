b878b454fa4ab4561fd05922bf757134bf82a73a
Batek Boczek <goterix1@gmail.com>
2017-03-21 19:30:15 +0200

    pomiar szumu

--- Project2/regulatorDiEmSi.m
+++ Project2/regulatorDiEmSi.m
@@ -3,13 +3,13 @@ clear all
 zadanie3;
 s=su;
 %params
-
+pomiar = 1;
 n = 2500; %dlugosc symulacji
 
-Yzad(1:n) = 1.9;
-Yzad(21:n) = 1.6;
-Yzad(1001:n)=1.4;
-Yzad(1501:n)=2.5;
+Yzad(1:20) = 0;
+Yzad(21:1000) = 1.6;
+Yzad(1001:1500)=1.4;
+Yzad(1501:2200)=2.5;
 Yzad(2201:n)=1.4;
 
 Z=zeros(1,n);
@@ -24,63 +24,88 @@ Y(1:n) = Ypp; %inicjalizacje tablic
 U(1:n) = Upp;
 u = U - Upp;
 err = 0;
-
-D=110; 
+szum = 0;
+D=110; Dz=50;
 %parametry regulatora dobrane eksperymentalnie
 % N=19; Nu=6; lambda=0.15;
 N=130.000000; Nu=6.000000; lambda=0.920000;
-
+for i=1:Dz
+   dz(i)=0;
+end
+Zpom = Z;
+if szum
+    Zpom = awgn(Zpom,snr); %dodaje bialy szum gaussowski o zadanym stosunku
+                          %sygna³u do szumu (SNR)
+end
 %inicjalizacja macierzy dUp
 for i=1:D-1
-   dup(i)=0;
+    dup(i)=0;
 end
 
 %generacja macierzy
 
 M=zeros(N,Nu);
 for i=1:N
-   for j=1:Nu
-      if (i>=j)
-         M(i,j)=s(i-j+1);
-      end;
-   end;
+    for j=1:Nu
+        if (i>=j)
+            M(i,j)=s(i-j+1);
+        end;
+    end;
 end;
 
 Mp=zeros(N,D-1);
 for i=1:N
-   for j=1:D-1
-      if i+j<=D
-         Mp(i,j)=s(i+j)-s(j);
-      else
-         Mp(i,j)=s(D)-s(j);
-      end;      
-   end;
+    for j=1:D-1
+        if i+j<=D
+            Mp(i,j)=s(i+j)-s(j);
+        else
+            Mp(i,j)=s(D)-s(j);
+        end;
+    end;
 end;
 
+Mz=zeros(N,Dz-1);
+for i=1:N
+    for j=1:Dz-1
+        if i+j<=Dz
+            Mz(i,j)=sz(i+j)-sz(j);
+        else
+            Mz(i,j)=sz(Dz)-sz(j);
+        end;
+    end;
+end;
+Mz = [sz(1:N) Mz];
 %przeksztalcanie wyliczonych macierzy do potrzebnych nam parametrow
 
 K=((M'*M+lambda*eye(Nu))^-1)*M';
 Ku=K(1,:)*Mp;
+Kz=K(1,:)*Mz;
 Ke=sum(K(1,:));
 
 %glowna petla
 
 for i=21:n
-   
-   Y(i)=symulacja_obiektu6y(U(i-6), U(i-7), Z(i-3) , Z(i-4), Y(i-1), Y(i-2));
-   
-   e=Yzad(i)-Y(i); %uchyb
-   err = err + e^2;
-   
-   du=Ke*e-Ku*dup'; %regulator
-   
-   for n=D-1:-1:2
-      dup(n)=dup(n-1);
-   end
-   dup(1)=du;
-   u(i)=u(i-1)+dup(1);
-  
-   U(i)=u(i)+Upp; %przesuniecie do punktu pracy
+    
+    Y(i)=symulacja_obiektu6y(U(i-6), U(i-7), Z(i-3) , Z(i-4), Y(i-1), Y(i-2));
+    
+    e=Yzad(i)-Y(i); %uchyb
+    err = err + e^2;
+    
+    du=Ke*e-Ku*dup'; %regulator
+    if pomiar %regulator nie bierze pod uwage pomiaru zaklocen gdy pomiar=0
+        du = du - Kz*dz';
+        for n=Dz:-1:2
+            dz(n)=dz(n-1);
+        end
+        dz(1)=Zpom(i)-Zpom(i-1);
+    end
+    for n=D-1:-1:2
+        dup(n)=dup(n-1);
+    end
+    dup(1)=du;
+    u(i)=u(i-1)+dup(1);
+    
+    U(i)=u(i)+Upp; %przesuniecie do punktu pracy
 end
 
 err
@@ -89,14 +114,14 @@ figure('Position',  [403 246 820 420]);
 title('obiekt z regulatorem PID');
 subplot('Position', [0.1 0.12 0.8 0.15]);
 stairs(U);
-ylabel('u'); 
+ylabel('u');
 xlabel('k');
 
 subplot('Position', [0.1 0.37 0.8 0.6]);
 plot(Y);
-ylabel('y'); 
+ylabel('y');
 
-hold on; 
+hold on;
 stairs(Yzad,':');
 %zapisywanie danych do plikow txt w celu narysowania wykresow w LATEXie
 nazwa = strcat('wykresy/zadanie6_DMC_Yzad.txt');
