cc730ae1e6fe9995eb8f5f67a0e934eca83089be
Batek Boczek <goterix1@gmail.com>
2017-03-26 19:27:23 +0300

    Merged

--- LAB1/src/aproks.m
+++ LAB1/src/aproks.m
@@ -4,15 +4,18 @@ T2=X(2);
 K=X(3);
 Td=10;
 y(1:350)=0;
-u(1:350)=1;
-
+ua(1:350)=0;
+u(1:340)=0;
+u(2:350)=22;
+close all
 tmp = load('zmienne.mat');
 Yskok = tmp.Yz;
 Ypp=ones(1,350)*30.81;
 OdpSkok = (Yskok-Ypp)/22;
-stairs(OdpSkok);
-nazwa = strcat('odp_skokowa.txt');
-savePlot(1:1:350,OdpSkok,nazwa);
+% stairs(OdpSkok);
+% hold on
+% nazwa = strcat('odp_skokowa.txt');
+% savePlot(1:1:350,OdpSkok,nazwa);
 alpha1 = exp(-1/T1);
 alpha2 = exp(-1/T2);
 a1 = -alpha1-alpha2;
@@ -22,12 +25,17 @@ b2 = K*(alpha1*T2*(1-alpha2)-alpha2*T1*(1-alpha1))/(T1-T2);
 
 for k = Td+3:350
 y(k) = b1*u(k - Td -1)+b2*u(k-Td-2)-a1*y(k-1)- a2*y(k-2);
+ua(k) = aproksymacjaObiektu(28,28,y(k-1),y(k-2));
 end
-hold on
 
- stairs(y)
- nazwa = strcat('funkcja_aproksymujaca.txt');
-savePlot(1:1:350,y,nazwa);
+% hold on
+Y=y+Ypp;
+stairs(Y)
+hold on 
+% stairs(ua)
+%  nazwa = strcat('funkcja_aproksymuj¹ca.txt');
+% savePlot(1:1:350,y,nazwa);
+
 e = OdpSkok' - y;
 E=(norm(e))^2;
 end
