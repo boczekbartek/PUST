354844c7167cf632cbee3d69305e47824c3e5602
Batek Boczek <goterix1@gmail.com>
2017-03-26 21:02:45 +0300

    src files added

---
+++ LAB1/src/dmcfun.m
@@ -0,0 +1,81 @@
+function err = dmcfun( param )
+
+tmp = load('s.mat');
+s = tmp.s;
+n = 10000; %dlugosc symulacji
+Ypp=30.87;
+Upp=28;
+Yzad(1:n) = Ypp;
+Yzad(21:n) = 20;
+Yzad(1001:n)=40;
+Yzad(1501:n)=30;
+Yzad(2201:n)=33;
+
+Y(1:n) = Ypp; %inicjalizacje tablic
+U(1:n) = Upp;
+u = U - Upp;
+err = 0;
+
+yzad = Yzad-Ypp;
+
+y=Y-Ypp;
+D=250;
+
+%inicjalizacja macierzy dUp
+for i=1:D-1
+   dup(i)=0;
+end
+
+%generacja pozostalych macierzy
+M=zeros(param(1),param(2));
+for i=1:param(1)
+   for j=1:param(2)
+      if (i>=j)
+         M(i,j)=s(i-j+1);
+      end;
+   end;
+end;
+
+Mp=zeros(param(1),D-1);
+for i=1:param(1)
+   for j=1:D-1
+      if i+j<=D
+         Mp(i,j)=s(i+j)-s(j);
+      else
+         Mp(i,j)=s(D)-s(j);
+      end;      
+   end;
+end;
+
+%przeksztalcanie wyliczonych macierzy do potrzebnych nam parametrow
+
+K=((M'*M+param(3)*eye(param(2)))^-1)*M';
+Ku=K(1,:)*Mp;
+Ke=sum(K(1,:));
+
+%glowna petla
+
+for i=21:n
+      y(i)=aproksymacjaObiektu(u(i-11),u(i-12),y(i-1),y(i-2));
+    
+    e=yzad(i)-y(i); %uchyb
+    err = err + e^2;
+    
+    du=Ke*e-Ku*dup'; %regulator
+    for n=D-1:-1:2
+        dup(n)=dup(n-1);
+    end
+    dup(1)=du;
+    u(i)=u(i-1)+dup(1);
+    
+    U(i)=u(i)+Upp;%przesuniecie do punktu pracy
+   
+    if U(i)>100
+        U(i)=100;
+    elseif U(i) <0
+        U(i)=0;
+    end
+end
+
+
+end
--- LAB1/src/odpSkok.m
+++ LAB1/src/odpSkok.m
@@ -1,10 +1,8 @@
-function E = aproks(X)
-T1=X(1);
-T2=X(2);
-K=X(3);
 Td=10;
-
-y(1:350)=0;
+T1=17.099506;
+T2=64.970974;
+K=0.263644;
+s(1:350)=0;
 ua(1:350)=0;
 u(1:340)=0;
 u(2:350)=1;
@@ -13,8 +11,8 @@ tmp = load('zmienne.mat');
 Yskok = tmp.Yz;
 Ypp=ones(1,350)*30.81;
 OdpSkok = (Yskok-Ypp)/22;
-stairs(OdpSkok);
-hold on
+% stairs(OdpSkok);
+% hold on
 % nazwa = strcat('odp_skokowa.txt');
 % savePlot(1:1:350,OdpSkok,nazwa);
 alpha1 = exp(-1/T1);
@@ -25,17 +23,14 @@ b1 = K*(T1*(1- alpha1)-T2*(1-alpha2))/(T1-T2);
 b2 = K*(alpha1*T2*(1-alpha2)-alpha2*T1*(1-alpha1))/(T1-T2);
 
 for k = Td+3:350
-y(k) = b1*u(k - Td -1)+b2*u(k-Td-2)-a1*y(k-1)- a2*y(k-2);
-ua(k) = aproksymacjaObiektu(1,28,y(k-1),y(k-2));
+s(k) = b1*u(k - Td -1)+b2*u(k-Td-2)-a1*s(k-1)- a2*s(k-2);
 end
 
 % hold on
-Y=y+Ypp;
-stairs(y)
-hold off
+
+% stairs(s)
+% hold off
 % stairs(ua)
 %  nazwa = strcat('funkcja_aproksymuj¹ca.txt');
 % savePlot(1:1:350,y,nazwa);
-e = OdpSkok' - y;
-E=(norm(e))^2;
-end
+
