17c036815b11968ee02f9b0169223aafb57abf6f
aleksander.piotrowski <apiotrowski@ubuntu>
2017-06-11 23:23:09 +0200

    fixed some bugs

--- Project5/src/zadanie6DMC.m
+++ Project5/src/zadanie6DMC.m
@@ -1,4 +1,4 @@
-function [ E ] = zadanie3DMC(D,N,Nu,lambda,mi)
+function [ E ] = zadanie6DMC(D,N,Nu,lambda,mi)
 
 D=round(D);
 N=round(N);
@@ -55,9 +55,6 @@ u=zeros(NU,czas);
 du=zeros(NU,czas);
 dUP=zeros((D-1)*NU,1);
 
-Y=zeros(N*NY,1);
-Yzad=zeros(N*NY,1);
-
 M=cell(N,Nu);
 
 for i=1:N
@@ -68,7 +65,32 @@ for i=1:N
              s31(i-j+1) s32(i-j+1) s33(i-j+1) s34(i-j+1)]};
       else
           M(i,j)={zeros(NY,NU)};
-      end
+      end% subplot(245)
+% stairs(y(1,:));
+% title({['błąd E= ',num2str(E)];['E1 = ',num2str(e1)];['mi = ', num2str(mi(1))]})
+% hold on;
+% stairs(yzad(1,:),'--')
+% legend('Y1(k)','Y_z_a_d_1(k)');
+% xlabel('k')
+% ylabel('Y1(k)')
+% 
+% subplot(246)
+% stairs(y(2,:));
+% title({['błąd E= ',num2str(E)];['E2 = ',num2str(e2)];['mi = ', num2str(mi(2))]})
+% hold on;
+% stairs(yzad(2,:),'--')
+% legend('Y2(k)','Y_z_a_d_2(k)');
+% xlabel('k')
+% ylabel('Y2(k)')
+% 
+% subplot(247)
+% stairs(y(3,:));
+% title({['błąd E= ',num2str(E)];['E3 = ',num2str(e3)];['mi = ', num2str(mi(3))]})
+% hold on;
+% stairs(yzad(3,:),'--')
+% legend('Y3(k)','Y_z_a_d_3(k)');
+% xlabel('k')
+% ylabel('Y3(k)')
    end
 end
 M=cell2mat(M);
@@ -83,7 +105,7 @@ for i=1:N
          MP(i,j)={[s11(D)-s11(j) s12(D)-s12(j) s13(D)-s13(j) s14(D)-s14(j);...
              s21(D)-s21(j) s22(D)-s22(j) s23(D)-s23(j) s24(D)-s24(j);...
              s31(D)-s31(j) s32(D)-s32(j) s33(D)-s33(j) s34(D)-s34(j)]};
-      end
+      end      
    end
 end
 MP=cell2mat(MP);
@@ -98,22 +120,41 @@ for i=1:Nu
     end
 end
 Lambda=cell2mat(Lambda);
-Mi=cell(N,N);
+MI=cell(N,N);
 for i=1:N
     for j=1:N
         if i==j
-            Mi(i,j)={diag(mi)};
+            MI(i,j)={diag(mi)};
         else
-            Mi(i,j)={zeros(NY,NY)};
+            MI(i,j)={zeros(NY,NY)};
         end
     end
 end
-Mi=cell2mat(Mi);
-K=(M'*Mi*M+Lambda)^(-1)*M'*Mi;
+
+MI=cell2mat(MI);
+
+K=(M'*MI*M+Lambda)^(-1)*M'*MI;
+
 K1=K(1:NU,:);
 
+ku=K1*MP;
+
+ke=zeros(4,3);
+
+ke(1,1)=sum(K(1,1:3:(N*NY)));
+ke(1,2)=sum(K(1,2:3:(N*NY)));
+ke(1,3)=sum(K(1,3:3:(N*NY)));
+ke(2,1)=sum(K(2,1:3:(N*NY)));
+ke(2,2)=sum(K(2,2:3:(N*NY)));
+ke(2,3)=sum(K(2,3:3:(N*NY)));
+ke(3,1)=sum(K(3,1:3:(N*NY)));
+ke(3,2)=sum(K(3,2:3:(N*NY)));
+ke(3,3)=sum(K(3,3:3:(N*NY)));
+ke(4,1)=sum(K(4,1:3:(N*NY)));
+ke(4,2)=sum(K(4,2:3:(N*NY)));
+ke(4,3)=sum(K(4,3:3:(N*NY)));
+
 for k=10:czas
-    
     [y(1,k),y(2,k),y(3,k)]=symulacja_obiektu10...
         (u(1,k-1),u(1,k-2),u(1,k-3),u(1,k-4),...
         u(2,k-1),u(2,k-2),u(2,k-3),u(2,k-4),...
@@ -123,24 +164,18 @@ for k=10:czas
         y(2,k-1),y(2,k-2),y(2,k-3),y(2,k-4),...
         y(3,k-1),y(3,k-2),y(3,k-3),y(3,k-4));
     
-    Y(1:3:(N*NY))=y(1,k);
-    Y(2:3:(N*NY))=y(2,k);
-    Y(3:3:(N*NY))=y(3,k);
-    
-    Yzad(1:3:(N*NY))=yzad(1,k);
-    Yzad(2:3:(N*NY))=yzad(2,k);
-    Yzad(3:3:(N*NY))=yzad(3,k);
-    
-    du(:,k)=K1*(Yzad-Y-MP*dUP);
+    du(:,k)=ke*(yzad(:,k)-y(:,k))-ku*dUP;
     
     for i=((D-1)*NU):-4:8
       dUP(i)=dUP(i-4);
       dUP(i-1)=dUP(i-5);
       dUP(i-2)=dUP(i-6);
       dUP(i-3)=dUP(i-7);
+    
     end
-   dUP(1:4)=du(:,k);
-   u(:,k)=u(:,k-1)+du(:,k);
+    dUP(1:4)=du(:,k);
+    u(:,k)=u(:,k-1)+du(:,k);
+    
 end
 
 e=yzad-y;
@@ -150,20 +185,21 @@ e3=sum(e(3,:).^2);
 E=e1+e2+e3;
 
 
-savePlot(1:czas,u(1,:),'zad4dmc_U1.txt');
-savePlot(1:czas,u(2,:),'zad4dmc_U2.txt');
-savePlot(1:czas,u(3,:),'zad4dmc_U3.txt');
-savePlot(1:czas,u(4,:),'zad4dmc_U4.txt');
 
-savePlot(1:czas,y(1,:),'zad4_Y1.txt');
-savePlot(1:czas,y(2,:),'zad4_Y2.txt');
-savePlot(1:czas,y(3,:),'zad4_Y3.txt');
+savePlot(1:czas,u(1,:),'zad6dmc_U1.txt');
+savePlot(1:czas,u(2,:),'zad6dmc_U2.txt');
+savePlot(1:czas,u(3,:),'zad6dmc_U3.txt');
+savePlot(1:czas,u(4,:),'zad6dmc_U4.txt');
 
-savePlot(1:czas,yzad(1,:),'zad4_Yzad1.txt');
-savePlot(1:czas,yzad(2,:),'zad4_Yzad2.txt');
-savePlot(1:czas,yzad(3,:),'zad4_Yzad3.txt');
+savePlot(1:czas,y(1,:),'zad6_Y1.txt');
+savePlot(1:czas,y(2,:),'zad6_Y2.txt');
+savePlot(1:czas,y(3,:),'zad6_Y3.txt');
 
-savePlot(0,0,sprintf('zad4_blad_E_%f.txt',E));
+savePlot(1:czas,yzad(1,:),'zad6_Yzad1.txt');
+savePlot(1:czas,yzad(2,:),'zad6_Yzad2.txt');
+savePlot(1:czas,yzad(3,:),'zad6_Yzad3.txt');
+
+savePlot(0,0,sprintf('zad6_blad_E_%f.txt',E));
 
 
 %     
@@ -217,5 +253,5 @@ savePlot(0,0,sprintf('zad4_blad_E_%f.txt',E));
 % legend('Y3(k)','Y_z_a_d_3(k)');
 % xlabel('k')
 % ylabel('Y3(k)')
-    
+
 end
