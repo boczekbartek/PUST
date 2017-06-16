5eae867acec2495a1bbce18f3fb1d97ba3c80a28
Bartłomiej Boczek <bartek1boczek@gmail.com>
2017-06-16 11:16:11 +0200

    Rysowanie odpowiedzi skokowych dla 50 regciów

---
+++ ProjectAdditional/src/rysowanieOdpSkok50reg.m
@@ -0,0 +1,17 @@
+Us = ones(1,50);
+Ue = ones(1,50);
+Us(1) = -1
+Ue(1) = -0.98;
+for m=0:47
+  Us(m+2) = -0.98 + m*0.0408333333333333;
+  Ue(m+2) = -0.98 + (m*0.0408333333333333) + 0.0408333333333333;
+end
+Us(50)=Ue(49);
+
+for m=1:2
+    Ustart = Us(m)
+    Uend = Ue(m)
+    figure
+    plot(odpowiedzi_skokowe(Ustart,Uend))
+    title(sprintf("Odpowiedź skokowa dla regulatora lokalnego \nz przedziału (%2.4f, %2.4f)",Ustart,Uend))
+end
