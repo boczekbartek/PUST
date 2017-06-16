45f835490784aab88dd7d8d2f0bb1a0c4181edb9
Bartłomiej Boczek <bartek1boczek@gmail.com>
2017-06-16 11:51:43 +0200

    cleanup, przydatne funkcje

---
+++ ProjectAdditional/src/rozmytych15optymalizacja.m
@@ -0,0 +1,22 @@
+
+clear all
+Us15 = ones(1,15);
+Ue15= ones(1,15);
+Us15(1) = -1
+Ue15(1) = -0.6;
+Ue15(end)=1
+for m=0:12
+  Us15(m+2) = -0.6+ m*0.0538461538461538;
+  Ue15(m+2) = -0.6 + (m*0.0538461538461538) + 0.0538461538461538;
+end
+Us15(end)=0.6;
+Ue15(end-1)=0.6;
+
+for m=1:50
+    m
+    Ustart = Us15(m)
+    Uend = Ue15(m)
+    tic; [ku(m,:),ke(m), e(m) ]=optymalizacja(Ustart,Uend);toc;
+    e(m)
+    save(sprintf("OptymalizacjaGa_15reg_iter=%d",m))
+end
--- ProjectAdditional/src/rozmytych50optymalizacja.m
+++ ProjectAdditional/src/rozmytych50optymalizacja.m
@@ -1,6 +1,10 @@
+
+clear all
+
+% Liczenie zakreów przedziałów
 Us = ones(1,50);
 Ue = ones(1,50);
-Us(1) = -1
+Us(1) = -1;
 Ue(1) = -0.98;
 for m=0:47
   Us(m+2) = -0.98 + m*0.0408333333333333;
@@ -11,7 +15,7 @@ Us(50)=Ue(49);
 for m=1:50
     Ustart = Us(m)
     Uend = Ue(m)
-    figure
-    plot(odpowiedzi_skokowe(Ustart,Uend))
-    title(sprintf("Odpowiedź skokowa dla regulatora lokalnego \nz przedziału (%2.4f, %2.4f)",Ustart,Uend))
+    tic; [ku(m,:),ke(m), e(m) ]=optymalizacja(Ustart,Uend);toc;
+    e(m)
+    save(sprintf("OptymalizacjaGa_2proba_iter=%d",m))
 end
