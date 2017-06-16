a5f4aba3f8bbfc61309146188db774788fb63c7c
Bartłomiej Boczek <bartek1boczek@gmail.com>
2017-06-16 11:42:13 +0200

    poprawione odp skokowe

--- ProjectAdditional/src/odpowiedzi_skokowe.m
+++ ProjectAdditional/src/odpowiedzi_skokowe.m
@@ -2,7 +2,7 @@ function s = odpowiedzi_skokowe(from, to)
         U(1:100) = from;
         U(20:100) = to;
         Y(1:100) = char_stat_fun(from);
-        for k = 21:100
+        for k = 7:100
             Y(k) = symulacja_obiektu10y(U(k-5), U(k-6), Y(k-1), Y(k-2));
         end
         s(1:80)=(Y(21:100)-char_stat_fun(from))/(to-from);
