bf80c02b718287e0a903b820a79ef5181ab3dcd5
Bartłomiej Boczek <bartek1boczek@gmail.com>
2017-06-16 09:06:54 +0200

    optymalizacja z ga

--- ProjectAdditional/src/optymalizacja.m
+++ ProjectAdditional/src/optymalizacja.m
@@ -1,11 +1,13 @@
 function [ku, ke] = optymalizacja(Ustart,Uend)
 odpSkok = odpowiedzi_skokowe(Ustart, Uend);
-x0 = [2 2 10];
+
+
 lb = [1, 1, 0.0001];
 ub = [80,90,1000];
-% [nastawy, min_error, exitfalg] = ga(@(x)(dmcfun(x,Ustart,Uend,odpSkok,false)),3,[], [], [],[],lb,ub,[],[]);
-[nastawy, min_error]=fmincon(@(x)(dmcfun(x,Ustart,Uend,odpSkok,false)),x0,[],[],[],[],lb,ub);
+
+[nastawy, min_error, exitfalg] = ga(@(x)(dmcfun(x,Ustart,Uend,odpSkok,false)),3,[], [], [],[],lb,ub,[],[]);
+% [nastawy, min_error]=fmincon(@(x)(dmcfun(x,Ustart,Uend,odpSkok,false)),x0,[],[],[],[],lb,ub);
 N = nastawy(1);
 Nu = nastawy(2);
 Lambda = nastawy(3);
-[e, ku, ke] = dmcfun(nastawy, Ustart,Uend,odpowiedzi_skokowe(Ustart,Uend), true);
+[e, ku, ke] = dmcfun(nastawy, Ustart,Uend,odpSkok, false);
