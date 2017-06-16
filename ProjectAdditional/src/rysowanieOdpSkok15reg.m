2a6b8991ae7aae0282b5b94327e9a4026e6d15fe
Alek Piotrowski <Alek Piotrowski>
2017-06-16 12:35:02 +0200

    poprawilem bledy z polskimi znakami

--- ProjectAdditional/src/rysowanieOdpSkok15reg.m
+++ ProjectAdditional/src/rysowanieOdpSkok15reg.m
@@ -16,5 +16,5 @@ for m=1:15
     Uend = Ue15(m);
     figure
     plot(odpowiedzi_skokowe(Ustart,Uend))
-    title(sprintf("Odpowiedź skokowa dla regulatora lokalnego \nz przedziału (%2.4f, %2.4f)",Ustart,Uend))
+    title(sprintf('Odpowiedz skokowa dla regulatora lokalnego \nz przedzialu (%2.4f, %2.4f)',Ustart,Uend))
 end
